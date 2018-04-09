1a3a4871ec55749bd0456bc9fdf39dd68aa84587
Kaila Goff <kaila.goff@gmail.com>
2018-04-08 21:09:35 -0700

    add final articles to database before deployment

--- src/app/gear/gear.component.ts
+++ src/app/gear/gear.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-gear',
   templateUrl: './gear.component.html',
-  styleUrls: ['./gear.component.css']
+  styleUrls: ['./gear.component.css'],
 })
-export class GearComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class GearComponent {
 
 }
