2d78d44772415ca51420cda969419a1b6cb28f43
Kaila Goff <kaila.goff@gmail.com>
2018-04-08 20:08:25 -0700

    add ability to only add,edit, or delete articles if logged in with admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Hike} from '../hike.model';
 import { HikeService } from '../hike.service';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
-  providers: [HikeService]
+  providers: [HikeService, AuthenticationService]
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  user;
+  public isLoggedIn: Boolean;
+  public userName: String;
 
-  constructor(private hikeService: HikeService) { }
+  constructor(private hikeService: HikeService, public authService: AuthenticationService) {
 
-  ngOnInit() {
-  }
+    this.authService.user.subscribe(user => {
+       if (user == null) {
+         this.isLoggedIn = false;
+       } else {
+         this.isLoggedIn = true;
+         this.userName = user.displayName;
+       }
+     });
+   }
+
+   login() {
+     this.authService.login();
+   }
+
+   logout() {
+     this.authService.logout();
+   }
 
   submitForm(title: string, location: string, distance: string, content: string, image: string, image2: string) {
    var newHike: Hike = new Hike(title, location, distance, content, image, image2);
